ba8bf9cdc296e4869c8133c806bf9f2faafcf131
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-02-22 10:51:21 +0200

    Added Table Fields to load

--- Main/src/com/schoolAdmin/controllers/user/Table_View.java
+++ Main/src/com/schoolAdmin/controllers/user/Table_View.java
@@ -18,7 +18,7 @@ import java.util.ResourceBundle;
 import com.schoolAdmin.app.App;
 import com.schoolAdmin.modals.AlertModule;
 import com.schoolAdmin.modals.MemberModel;
-import com.schoolAdmin.database.Sqlite;
+import com.schoolAdmin.database.Psql;
 import com.schoolAdmin.controllers.misc.SceneCtrl;
 // import com.schoolAdmin.controllers.admin.UpdateCtrl;
 import javafx.stage.FileChooser;
@@ -29,16 +29,24 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Row;
 import java.nio.file.*;
 
-public class Table_View {
+public class Table_View implements Initializable {
+    Stage stage = new Stage();
+    Psql psql = new Psql();
+
+    Window owner = stage.getOwner();
+    SceneCtrl scene_switcher = new SceneCtrl();
     //Start Of Table Properties
     @FXML
-    private TableView<MemberModel> pqslTable;
+    private TableView<MemberModel> psqlTable;
 
     @FXML
     private TableColumn<MemberModel, String> id_;
 
     @FXML
     private TableColumn<MemberModel, String> fname;
+
+    @FXML
+    private TableColumn<MemberModel, String> lname;
 
     @FXML
     private TableColumn<MemberModel, String> Title_;
@@ -75,6 +83,9 @@ public class Table_View {
 
     @FXML
     private TableColumn<MemberModel, String> MobilePhone;
+
+    @FXML
+    private TableColumn<MemberModel, String> Email;
 
     @FXML
     private TableColumn<MemberModel, String> Salvation;
@@ -229,5 +240,43 @@ public class Table_View {
     //Button Start
     @FXML
     private Button subQuery;
+
+    ObservableList<MemberModel>records;
+
+    @Override
+    public void initialize(URL location, ResourceBundle bundle){
+        assert psqlTable !=null : "Failed to load table";
+        
+        id_.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("id_"));
+        fname.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("fname"));
+        lname.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("lname"));
+        Title_.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Title_"));
+        Address.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Address"));
+        DOB_.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("DOB_"));
+        DateJoined_.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("DateJoined_"));
+        Dept.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Dept"));
+        DeptLeader.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("DeptLeader"));
+        HomeGroup.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("HomeGroup"));
+        Homephone.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Homephone"));
+        IDNum.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("IDNum"));
+        KidsNo_.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("KidsNo_"));
+        MStatus_.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("MStatus_"));
+        MobilePhone.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("MobilePhone"));
+        Email.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Email"));
+        Salvation.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Salvation"));
+        Sex.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Sex"));
+        waterBapt.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("waterBapt"));
+        SpiritBapt.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("SpiritBapt"));
+        Surbub.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("Surbub"));
+        WorkPhone.setCellValueFactory(new PropertyValueFactory<MemberModel, String>("WorkPhone"));
+        
+        try {
+            records = loadTable();
+            table_Listener();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+    }
 
 }
