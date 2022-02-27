22b6991aa80d299d75cd722a8e18a62e69c75a51
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-02-27 16:36:10 +0200

    Committed Clean Files

---
+++ src/main/java/com/cm/cm/Launcher.java
@@ -0,0 +1,11 @@
+package com.cm.cm;
+
+import com.cm.cm.app.App;
+
+import java.sql.SQLException;
+
+public class Launcher {
+    public static void main(String[] args) throws SQLException {
+        App.main(args);
+    }
+}
